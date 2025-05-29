7b68ea5a2093c546e5307aa65b4bca2efa50a8b8
ckc102 <ivanccc2015@gmail.com>
2025-05-29 20:47:50 +0800

    MF1 + CustomerDetailForm + SupplierDetailForm 29/5/2025

---
+++ Client/MF1.Designer.cs
@@ -0,0 +1,448 @@
+﻿namespace Client
+{
+    partial class MF1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            tabPage2 = new TabPage();
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            dataGridView1 = new DataGridView();
+            tabPage3 = new TabPage();
+            button7 = new Button();
+            button6 = new Button();
+            button5 = new Button();
+            button4 = new Button();
+            textBox2 = new TextBox();
+            label2 = new Label();
+            dataGridView2 = new DataGridView();
+            tabPage4 = new TabPage();
+            tabPage5 = new TabPage();
+            tabPage6 = new TabPage();
+            tabPage7 = new TabPage();
+            tabPage8 = new TabPage();
+            tabPage9 = new TabPage();
+            tabPage10 = new TabPage();
+            tabPage11 = new TabPage();
+            tabPage12 = new TabPage();
+            tabPage13 = new TabPage();
+            dataGridView3 = new DataGridView();
+            label3 = new Label();
+            textBox3 = new TextBox();
+            button8 = new Button();
+            button9 = new Button();
+            tabControl1.SuspendLayout();
+            tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            tabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).BeginInit();
+            tabPage4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView3).BeginInit();
+            SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Controls.Add(tabPage3);
+            tabControl1.Controls.Add(tabPage4);
+            tabControl1.Controls.Add(tabPage5);
+            tabControl1.Controls.Add(tabPage6);
+            tabControl1.Controls.Add(tabPage7);
+            tabControl1.Controls.Add(tabPage8);
+            tabControl1.Controls.Add(tabPage9);
+            tabControl1.Controls.Add(tabPage10);
+            tabControl1.Controls.Add(tabPage11);
+            tabControl1.Controls.Add(tabPage12);
+            tabControl1.Controls.Add(tabPage13);
+            tabControl1.Location = new Point(0, 0);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(1233, 533);
+            tabControl1.TabIndex = 0;
+            tabControl1.Tag = "";
+            // 
+            // tabPage1
+            // 
+            tabPage1.Location = new Point(4, 24);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(1225, 505);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "Dashboard";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Controls.Add(button3);
+            tabPage2.Controls.Add(button2);
+            tabPage2.Controls.Add(button1);
+            tabPage2.Controls.Add(textBox1);
+            tabPage2.Controls.Add(label1);
+            tabPage2.Controls.Add(dataGridView1);
+            tabPage2.Location = new Point(4, 24);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(1225, 505);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "Customers";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(534, 44);
+            button3.Name = "button3";
+            button3.Size = new Size(113, 23);
+            button3.TabIndex = 5;
+            button3.Text = "Delete Selected";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(397, 44);
+            button2.Name = "button2";
+            button2.Size = new Size(113, 23);
+            button2.TabIndex = 4;
+            button2.Text = "Edit Selected";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(260, 44);
+            button1.Name = "button1";
+            button1.Size = new Size(113, 23);
+            button1.TabIndex = 3;
+            button1.Text = "Add Customer";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(19, 44);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(179, 23);
+            textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(19, 12);
+            label1.Name = "label1";
+            label1.Size = new Size(179, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Input customer phone to search:";
+            label1.Click += label1_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(19, 95);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(1211, 403);
+            dataGridView1.TabIndex = 0;
+            // 
+            // tabPage3
+            // 
+            tabPage3.Controls.Add(button7);
+            tabPage3.Controls.Add(button6);
+            tabPage3.Controls.Add(button5);
+            tabPage3.Controls.Add(button4);
+            tabPage3.Controls.Add(textBox2);
+            tabPage3.Controls.Add(label2);
+            tabPage3.Controls.Add(dataGridView2);
+            tabPage3.Location = new Point(4, 24);
+            tabPage3.Name = "tabPage3";
+            tabPage3.Size = new Size(1225, 505);
+            tabPage3.TabIndex = 12;
+            tabPage3.Text = "Suppliers";
+            tabPage3.UseVisualStyleBackColor = true;
+            tabPage3.Click += tabPage3_Click;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(779, 49);
+            button7.Name = "button7";
+            button7.Size = new Size(113, 23);
+            button7.TabIndex = 7;
+            button7.Text = "Delete Selected";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(259, 49);
+            button6.Name = "button6";
+            button6.Size = new Size(113, 23);
+            button6.TabIndex = 6;
+            button6.Text = "Add Supplier";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(411, 49);
+            button5.Name = "button5";
+            button5.Size = new Size(113, 23);
+            button5.TabIndex = 5;
+            button5.Text = "Edit Selected";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(559, 49);
+            button4.Name = "button4";
+            button4.Size = new Size(194, 23);
+            button4.TabIndex = 4;
+            button4.Text = "Deactivate/Activate Selected";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(20, 49);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(179, 23);
+            textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(20, 10);
+            label2.Name = "label2";
+            label2.Size = new Size(171, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Input supplier phone to search:";
+            // 
+            // dataGridView2
+            // 
+            dataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView2.Location = new Point(6, 96);
+            dataGridView2.Name = "dataGridView2";
+            dataGridView2.Size = new Size(1211, 403);
+            dataGridView2.TabIndex = 1;
+            // 
+            // tabPage4
+            // 
+            tabPage4.Controls.Add(button9);
+            tabPage4.Controls.Add(button8);
+            tabPage4.Controls.Add(textBox3);
+            tabPage4.Controls.Add(label3);
+            tabPage4.Controls.Add(dataGridView3);
+            tabPage4.Location = new Point(4, 24);
+            tabPage4.Name = "tabPage4";
+            tabPage4.Padding = new Padding(3);
+            tabPage4.Size = new Size(1225, 505);
+            tabPage4.TabIndex = 2;
+            tabPage4.Text = "Product & Design Requests";
+            tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // tabPage5
+            // 
+            tabPage5.Location = new Point(4, 24);
+            tabPage5.Name = "tabPage5";
+            tabPage5.Size = new Size(1225, 505);
+            tabPage5.TabIndex = 3;
+            tabPage5.Text = "Quotations";
+            tabPage5.UseVisualStyleBackColor = true;
+            // 
+            // tabPage6
+            // 
+            tabPage6.Location = new Point(4, 24);
+            tabPage6.Name = "tabPage6";
+            tabPage6.Size = new Size(1225, 505);
+            tabPage6.TabIndex = 4;
+            tabPage6.Text = "Customer Orders";
+            tabPage6.UseVisualStyleBackColor = true;
+            // 
+            // tabPage7
+            // 
+            tabPage7.Location = new Point(4, 24);
+            tabPage7.Name = "tabPage7";
+            tabPage7.Size = new Size(1225, 505);
+            tabPage7.TabIndex = 5;
+            tabPage7.Text = "Production Orders";
+            tabPage7.UseVisualStyleBackColor = true;
+            // 
+            // tabPage8
+            // 
+            tabPage8.Location = new Point(4, 24);
+            tabPage8.Name = "tabPage8";
+            tabPage8.Size = new Size(1225, 505);
+            tabPage8.TabIndex = 6;
+            tabPage8.Text = "Shipments";
+            tabPage8.UseVisualStyleBackColor = true;
+            // 
+            // tabPage9
+            // 
+            tabPage9.Location = new Point(4, 24);
+            tabPage9.Name = "tabPage9";
+            tabPage9.Size = new Size(1225, 505);
+            tabPage9.TabIndex = 7;
+            tabPage9.Text = "Inventory";
+            tabPage9.UseVisualStyleBackColor = true;
+            // 
+            // tabPage10
+            // 
+            tabPage10.Location = new Point(4, 24);
+            tabPage10.Name = "tabPage10";
+            tabPage10.Size = new Size(1225, 505);
+            tabPage10.TabIndex = 8;
+            tabPage10.Text = "Purchase Orders";
+            tabPage10.UseVisualStyleBackColor = true;
+            // 
+            // tabPage11
+            // 
+            tabPage11.Location = new Point(4, 24);
+            tabPage11.Name = "tabPage11";
+            tabPage11.Size = new Size(1225, 505);
+            tabPage11.TabIndex = 9;
+            tabPage11.Text = "Service Cases";
+            tabPage11.UseVisualStyleBackColor = true;
+            // 
+            // tabPage12
+            // 
+            tabPage12.Location = new Point(4, 24);
+            tabPage12.Name = "tabPage12";
+            tabPage12.Size = new Size(1225, 505);
+            tabPage12.TabIndex = 10;
+            tabPage12.Text = "Admin / User Management";
+            tabPage12.UseVisualStyleBackColor = true;
+            // 
+            // tabPage13
+            // 
+            tabPage13.Location = new Point(4, 24);
+            tabPage13.Name = "tabPage13";
+            tabPage13.Size = new Size(1225, 505);
+            tabPage13.TabIndex = 11;
+            tabPage13.Text = "System Settings";
+            tabPage13.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView3
+            // 
+            dataGridView3.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView3.Location = new Point(3, 96);
+            dataGridView3.Name = "dataGridView3";
+            dataGridView3.Size = new Size(1211, 403);
+            dataGridView3.TabIndex = 1;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(21, 23);
+            label3.Name = "label3";
+            label3.Size = new Size(179, 15);
+            label3.TabIndex = 2;
+            label3.Text = "Input customer phone to search:";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(21, 55);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(179, 23);
+            textBox3.TabIndex = 4;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(253, 54);
+            button8.Name = "button8";
+            button8.Size = new Size(113, 23);
+            button8.TabIndex = 7;
+            button8.Text = "Add Supplier";
+            button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            button9.Location = new Point(383, 54);
+            button9.Name = "button9";
+            button9.Size = new Size(113, 23);
+            button9.TabIndex = 8;
+            button9.Text = "Add Supplier";
+            button9.UseVisualStyleBackColor = true;
+            // 
+            // MF1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1237, 534);
+            Controls.Add(tabControl1);
+            Name = "MF1";
+            Text = "MF1";
+            tabControl1.ResumeLayout(false);
+            tabPage2.ResumeLayout(false);
+            tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            tabPage3.ResumeLayout(false);
+            tabPage3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).EndInit();
+            tabPage4.ResumeLayout(false);
+            tabPage4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView3).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
+        private TabPage tabPage3;
+        private TabPage tabPage4;
+        private TabPage tabPage5;
+        private TabPage tabPage6;
+        private TabPage tabPage7;
+        private TabPage tabPage8;
+        private TabPage tabPage9;
+        private TabPage tabPage10;
+        private TabPage tabPage11;
+        private TabPage tabPage12;
+        private TextBox textBox1;
+        private Label label1;
+        private DataGridView dataGridView1;
+        private Button button2;
+        private Button button1;
+        private Button button3;
+        private TabPage tabPage20;
+        private TabPage tabPage14;
+        private TabPage tabPage13;
+        private DataGridView dataGridView2;
+        private Label label2;
+        private Button button7;
+        private Button button6;
+        private Button button5;
+        private Button button4;
+        private TextBox textBox2;
+        private Button button9;
+        private Button button8;
+        private TextBox textBox3;
+        private Label label3;
+        private DataGridView dataGridView3;
+    }
+}
---
+++ Client/SupplierDetailForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Client
+{
+    public partial class SupplierDetailForm : Form
+    {
+        public SupplierDetailForm()
+        {
+            InitializeComponent();
+        }
+
+        private void SupplierDetailForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
